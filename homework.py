f1b95c8319919b2402d17483326c698c1da72890
Clay Wightman <claywightman@gmail.com>
2020-03-30 14:14:52 -0400

    Json data is now flattened when being put into the database meaning it can now accept the 'to' and 'from' parameters properly.  I added a 'create table' method which is not implemented, but it shows the structure of the table I am using.  My next step is looking into a better solution to 'busy-wait' for checking if the json file is updated.

--- homework.py
+++ homework.py
@@ -1,11 +1,74 @@
-import json
-import os, os.path
-from datetime import datetime
-import requests
-import time
-import mysql.connector
-import db_config
-#Only need to connect if actually adding to server
+#/home/clay/Programs/homework/json_files
+import json                     #for converting json objects into python dicts
+import os, os.path              #for file naviagation
+from datetime import datetime   #for the error logger
+import requests                 #for downloading the PDF's by their URL
+import time                     #for implementing "busy wait" to cut down on CPU overhead
+import mysql.connector          #for connecting to a mySQL database
+import db_config                #my own personal file which includes database setup information.  This is not necessary for other users.
+
+
+
+
+
+#Should only be used if table does not already exist.  Included mainly to show the datatypes I used for each json object as well as the structure of my table
+def create_table(mycursor):
+    mycursor.execute("CREATE TABLE metadata (id VARCHAR(255) NOT NULL, \
+        to_name VARCHAR(255), to_company VARCHAR(255), to_address_zip VARCHAR(10), to_address_city VARCHAR(255), to_address_line1 VARCHAR(255), to_address_line2 VARCHAR(255), to_address_state VARCHAR(255), to_address_country VARCHAR(255),\
+        from_name VARCHAR(255),from_company VARCHAR(255), from_address_zip VARCHAR(10), from_address_city VARCHAR(255), from_address_line1 VARCHAR(255), from_address_line2 VARCHAR(255), from_address_state VARCHAR(255), from_address_country VARCHAR(255),\
+            size VARCHAR(255), object VARCHAR(255), imb_code VARCHAR(255), priority INT, mail_sort VARCHAR(255), mail_type VARCHAR(255), press_proof VARCHAR(255), file_created_at VARCHAR(255), PRIMARY KEY (id)")
+
+#Where the info in the .json file is actually inserted into the database
+def insert_in_database(json_dict, mycursor, mydb):
+    print(json_data['to']['name'])
+    sql = "INSERT INTO metadata (id,to_name,to_company,to_address_zip,to_address_city,to_address_line1,to_address_line2,to_address_state,to_address_country,from_name,from_company,from_address_zip,from_address_city,from_address_line1,from_address_line2,from_address_state,from_address_country,size,object,imb_code,priority,mail_sort,mail_type,press_proof,file_created_at) "\
+        "VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"\
+             %(json_dict['id'],json_dict['to']['name'],json_dict['to']['company'],json_dict['to']['address_zip'],json_dict['to']['address_city'],json_dict['to']['address_line1'],json_dict['to']['address_line2'],json_dict['to']['address_state'],json_dict['to']['address_country'],\
+                 json_dict['from']['name'],json_dict['from']['company'],json_dict['from']['address_zip'],json_dict['from']['address_city'],json_dict['from']['address_line1'],json_dict['from']['address_line2'],json_dict['from']['address_state'],json_dict['from']['address_country'],\
+                 json_dict['size'],json_dict['object'],json_dict['imb_code'],\
+                 json_dict['priority'],json_dict['mail_sort'],json_dict['mail_type'],json_dict['press_proof'],json_dict['file_created_at'])
+    mycursor.execute(sql)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted")
+
+#Creates a directory in the same folder as this project which is used to store the PDF files if one does not already exist
+def create_pdf_dir(base_path):
+    if not os.path.exists(pdf_dir_path):
+        os.mkdir(pdf_dir_path)
+
+#Gets the path for the directory full of .json files from the user.  If not a valid path it loops.
+def get_json_path():
+    path = input("Please enter the path of the directory you wish to track: \n")
+    while not os.path.isdir(path):
+        path = input("I could not find that directory, please try again\n")
+    return path
+
+#determines if the .json file is valid using the json.loads() function.
+def is_valid_json_format(file):
+    try:
+        json_data = json.loads(file)
+        return True
+    except ValueError as e:
+        return False
+
+#Checks the status code of the page the url points to.  This is not perfect as non-pdf pages can return a positive status code, but it at least doesn't try to download from pages that don't exist
+def is_valid_pdf_url(pdf_url):
+    r = requests.get(pdf_url)
+    if r.status_code == requests.codes.ok:
+        return True
+    else:
+        return False
+
+#Downloads the PDF (pretty self explanatory)
+def download_pdf(pdf_url, pdf_id):
+    pdf_file = requests.get(pdf_url)
+    open(os.path.join(pdf_dir_path, pdf_id),'wb').write(pdf_file.content)
+
+
+
+
+
+#database setup information.
 mydb = mysql.connector.connect(
     host = db_config.host,
     user = db_config.user,
@@ -14,63 +77,17 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-#/home/clay/Programs/homework/json_files
+
+#Initialize variables
 pdf_count = 0
 pdf_dir_path = os.path.join(os.getcwd(), "press_proofs")
 err_file_path = os.path.join(os.getcwd(), "error_log.txt")
-already_submitted_files = {}#dictionary to keep track of files already scanned.
-#======Gets a valid path to the directory from the user======
-
-def insert_in_database(json_dict, mycursor, mydb):
-    #Well now i know, use '%s' for values which are strings and %s (without quotation marks) for integers.  (youve gotta be kidding me)  I'll double check this with a clean example because w3schools is showing something different.
-    sql = "INSERT INTO metadata (id,size,object,imb_code,priority,mail_sort,mail_type,press_proof,file_created_at) "\
-        "VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s')"\
-             %(json_dict['id'],json_dict['size'],json_dict['object'],json_dict['imb_code'],\
-                 json_dict['priority'],json_dict['mail_sort'],json_dict['mail_type'],json_dict['press_proof'],json_dict['file_created_at'])
-             #Spent way too long trying to get string formatter %s to work.  Still not sure why it wouldn't.  Use this instead.
-    print(sql)
-    mycursor.execute(sql)
-    mydb.commit()
-    print("info inserted correctly")
-    print(mycursor.rowcount, "record inserted")
-
-def create_pdf_dir(base_path):
-    if not os.path.exists(pdf_dir_path):
-        os.mkdir(pdf_dir_path)
-
-def get_json_path():
-    path = input("Please enter the path of the directory you wish to track: \n")
-    while not os.path.isdir(path):
-        path = input("I could not find that directory, please try again\n")
-    return path
-
-def is_valid_json_format(file):
-    try:
-        json_data = json.loads(file)
-        return True
-    except ValueError as e:
-        return False
-
-def is_valid_pdf_url(pdf_url):
-    r = requests.get(pdf_url)
-    if r.status_code == requests.codes.ok:
-        return True
-    else:
-        return False
-
-def download_pdf(pdf_url, pdf_id):
-    pdf_file = requests.get(pdf_url)
-    open(os.path.join(pdf_dir_path, pdf_id),'wb').write(pdf_file.content)
-
-
-"""
-This first block of code is for dealing with files which are already
-inside of the json directory when the program initializes
-"""
-
+already_submitted_files = {}
 usr_input_path = get_json_path()
 create_pdf_dir(pdf_dir_path)
 num_files_in_json_dir = len([name for name in os.listdir(usr_input_path)])
+
+#This first block of code is for dealing with files which are already inside of the json directory when the program initializes
 for filename in os.listdir(usr_input_path):
     already_submitted_files[filename] = True 
     if filename.endswith(".json"):
@@ -102,15 +119,13 @@ for filename in os.listdir(usr_input_path):
         current_time = datetime.now().strftime("%H:%M:%S")
         err_f.write(current_time + ":  " + filename + " does not have the proper .json file extension\n")
 
-"""
-This second block of code is for files which are added while this program is running
-"""
 
+#This second block of code is for files which are added while this program is running
 print("Number of files is " + str(num_files_in_json_dir))
-while True: #Maybe use watchdog?  Maybe use threading (Check bookmark blog)
-    if num_files_in_json_dir != len([name for name in os.listdir(usr_input_path)]): #maybe change != to >
+while True:
+    if num_files_in_json_dir != len([name for name in os.listdir(usr_input_path)]):
         for filename in os.listdir(usr_input_path): 
-            if not filename in already_submitted_files: #Look into making this faster.  Might be able to do a lookup and see if it return null?  Otherwise might as well just use an array (looked into it, may have broke it) Might also be worth looking into querying the database to see if the value already exists.  Not sure about time complexity of that though
+            if not filename in already_submitted_files:
                 already_submitted_files[filename] = True
                 if filename.endswith(".json"):
                     f = open(os.path.join(usr_input_path, filename),'r')
@@ -154,13 +169,14 @@ while True: #Maybe use watchdog?  Maybe use threading (Check bookmark blog)
 
 
 #TODO
+#Change '%s' to %s for int variables in database
 #Make sure your code is protected from sql injection?  (Maybe since JSON is likey user created?)
 #Probably make the database relational for nested .json objects
 #Change the database to a SQL database
 #Test this code on a windows machine.  There might be an unexpected area where my paths don't work as expected (Thanks to windows dang backslashes!)
 #Make the event listener for a change in number of files more CPU efficient.  Python has a "Watchdog" class which might be worth looking into, as well as threading (Check your bookmarks)
 #Make a seperate file holding information about which files have already been scanned in, that way if you close and restart the program it doesn't 
-#   auto push already uploaded files to the database
+#auto push already uploaded files to the database
 #Check with Andrew and see if I should make each ID unique (Right now it will take any ID to the database and not treat it as a unique identifier)
 
 
@@ -168,6 +184,9 @@ while True: #Maybe use watchdog?  Maybe use threading (Check bookmark blog)
 
 
 #toDONE
+#I am not creating the table in the database in this code for two reasons
+#   1)I am assuming the table will already exist in the database
+#   2)Because of the limited number of test .json files I have I do not want to create a table with ID set to primary key since it will make me unable to upload files
 #Add a folder to my gitignore which contains sensitive database information
 #Make the program more modular.  See what sections can be their own functions, its too garbled right now.
 #actually upload the JSON objects to a database.  I'm still not sure if i should use noSQL or SQL databases though. (I ended up using MongoDB Atlas because its free and I am more familiar with it)
